4b57da3231efdbabb7813570b6d905342720dfbe
Tanaka-2204 <khoabdse170432@fpt.edu.vn>
2024-10-31 08:13:39 +0700

    Update useOrchid.js

--- src/components/hook/useOrchid.js
+++ src/components/hook/useOrchid.js
@@ -1,10 +1,21 @@
 import { useEffect, useState } from "react";
-import { ListOfOrchids } from "../Orchid/ListOfOrchids";
 
 export const useOrchid = () => {
   const [orchidList, setOrchidList] = useState([]);
+  const baseURL = `https://670c68177e5a228ec1d03b5e.mockapi.io/orchids`;
+
+  const fetchAPI = async () => {
+
+      const response = await fetch(baseURL);
+      
+      const data = await response.json();
+      setOrchidList(data);
+    
+  };
+
   useEffect(() => {
-    setOrchidList(ListOfOrchids);
+    fetchAPI();
   }, []);
+
   return orchidList;
 };
